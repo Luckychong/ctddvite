33514971e93bbb93c9387655d9a9312c205dc200
Luckychong <43589909+Luckychong@users.noreply.github.com>
2025-04-02 21:07:56 -0700

    updated todolist

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,4 @@
 import React, { useState } from "react";
-import "./App.css"; 
-import TodoList from "./TodoList"; 
-import TodoForm from "./TodoForm"; 
 
 function App() {
   const [todos, setTodos] = useState([
@@ -10,23 +7,26 @@ function App() {
     { id: 3, title: "code out app" },
   ]);
 
-  
-  const [newTodo, setNewTodo] = useState(""); 
+  const [newTodo, setNewTodo] = useState("");
 
-  
   const addTodo = (title) => {
     const newTodoItem = {
-      id: todos.length + 1, 
+      id: todos.length + 1,
       title: title,
     };
-    setTodos([...todos, newTodoItem]); 
+    setTodos([...todos, newTodoItem]);
   };
 
   return (
     <div>
       <h1>Todo List</h1>
-      <TodoForm addTodo={addTodo} setNewTodo={setNewTodo} newTodo={newTodo} />git
+    
+      <TodoForm addTodo={addTodo} setNewTodo={setNewTodo} newTodo={newTodo} />
+
+    
       <TodoList todos={todos} />
+
+    
       <p>{newTodo}</p>
     </div>
   );
